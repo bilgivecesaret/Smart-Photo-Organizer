641fbf00c183d65741a252353bb7324645779adf
İdris AYDIN <121408791+idris123aydin@users.noreply.github.com>
2026-01-28 19:22:57 +0300

    Create AutoAlbum.java

---
+++ app/src/main/java/com/example/smart_photo_organizer/model/AutoAlbum.java
@@ -0,0 +1,13 @@
+package com.example.smart_photo_organizer.model;
+
+import java.util.List;
+
+public class AutoAlbum {
+    public String title;
+    public List<HashItem> photos;
+
+    public AutoAlbum(String title, List<HashItem> photos) {
+        this.title = title;
+        this.photos = photos;
+    }
+}
